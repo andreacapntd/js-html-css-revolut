e960fab9804775f49c3866945a70ff66db9e4b02
Andrea Caprara <andreacaprara85@gmail.com>
2020-07-07 16:14:05 +0200

    dropdown avvicinato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.parent-down').hover(function() {
+    console.log($(this).children());
     $(this).children('.header-menu-icon').toggleClass('hidden', 'active');
   });
 });
