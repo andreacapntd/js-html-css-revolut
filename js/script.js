1b7d9654de4ed65850ff0171b3b0eb12173b6647
andreacapntd <andreacapntd@gmail.com>
2020-07-08 14:40:17 +0200

    add content hamburger menu and add js click function

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     console.log($(this).children());
     $(this).children('.header-menu-icon').toggleClass('hidden', 'active');
   });
+
+  $('#item-chevron').click(function() {
+    $(this).next('.drop').slideToggle('fast');
+  });
 });
