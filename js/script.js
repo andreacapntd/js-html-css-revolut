add6a30fdaebb2fce8e7982915fdda2e6a646345
andreacapntd <andreacapntd@gmail.com>
2020-07-08 15:17:19 +0200

    js ok

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ $(document).ready(function() {
     $(this).children('.header-menu-icon').toggleClass('hidden', 'active');
   });
 
-  $('#item-chevron').click(function() {
+  $('i#item-chevron').click(function() {
     $(this).next('.drop').slideToggle('fast');
+  });
+
+  $("i.fa-bars").click(function (){
+  $(".hamburger-menu").toggleClass('hidden', 'active');
+  });
+
+
+  $("i.fa-times").click(function(){
+  $(".hamburger-menu").toggleClass('hidden', 'active');
   });
 });
